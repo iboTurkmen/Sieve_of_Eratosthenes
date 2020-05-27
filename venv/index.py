f6993d4ad837a560198a8b62579dd59d1e88d829
iboturkmen <iboTurkmen92@outlook.com>
2020-05-27 23:34:05 +0300

    take the limit input that will be find prime number until that number

--- venv/index.py
+++ venv/index.py
@@ -1,1 +1,22 @@
-print("stay home")
+
+
+
+
+
+
+
+def main():
+    print("For Exit please enter 'EXIT' or 'exit'")
+    inputNum = input("Please Enter Limit of a Prime Number That You Want: ")
+    if inputNum.upper() == "EXIT":
+        exit()
+    if inputNum == "":
+        main()
+    if int(inputNum) < 2:
+        print("Please Enter Positive Numbers only and bigger than 2!!!")
+        main()
+    else:
+        checkNum(inputNum)
+
+
+main()
